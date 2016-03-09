c3f21bb960944ab37e70427b4b424d56f15191eb
Renato Rodrigues <renato.mello@paulistano.org.br>
2016-03-09 17:08:38 -0300

    Aula 06

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,17 @@
 angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
-.config(function($routeProvider) {
+.config(function($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
 
     $routeProvider.when('/fotos',{
         templateUrl: 'partials/principal.html',
         controller: 'FotosController'
     });
 
+    $routeProvider.when('/fotos/new',{
+        templateUrl: 'partials/foto.html',
+        controller: 'FotoContoller'
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/fotos'});
 });
